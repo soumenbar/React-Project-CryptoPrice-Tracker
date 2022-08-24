4435b1746130433aa4d94bbdb032ca95454b32bb
soumenbar <barsoumen33@gmail.com>
2022-08-24 17:52:01 +0530

    Second

--- src/App.js
+++ src/App.js
@@ -12,8 +12,11 @@ function App() {
   useEffect(()=>{
     axios
     .get(
-      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+
+      
     )
+
     .then(res => {
       setCoins(res.data);
       console.log(res.data);
